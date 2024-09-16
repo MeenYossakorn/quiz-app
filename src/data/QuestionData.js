7d72220786786ed7f9754fc6af69a5f0a200f331
MeenYossakorn <143907645+MeenYossakorn@users.noreply.github.com>
2024-09-16 11:38:04 +0700

    create (menu,quiz,score,questiondata)
    create (menu ,quiz , score , questiondata)

---
+++ src/data/QuestionData.js
@@ -0,0 +1,27 @@
+const QuestionData = [
+  {
+    question: "ข้อใดไม่ใช่ tag html",
+    A: "<react>",
+    B: "<a>",
+    C: "<p>",
+    D: "<h1>",
+    Answer: "A",
+  },
+  {
+    question: "ข้อถูกต้องเกี่ยวกับ React",
+    A: "ทำงานฝั่ง server",
+    B: "ใช้จัดการฐานข้อมูล",
+    C: "เป็น JavaScript Library",
+    D: "ถูกทุกข้อ",
+    Answer: "C",
+  },
+  {
+    question: "ข้อใดคือชื่าอ Browser",
+    A: "PHP",
+    B: "React",
+    C: "MySQL",
+    D: "Chrome",
+    Answer: "D",
+  },
+];
+export default QuestionData;
